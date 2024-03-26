430edb29bbf76da862c9d556f159b54061c048f2
shanestorks <shanestorks@gmail.com>
2024-03-26 13:56:22 -0400

    troubleshooting BLIP2

--- travel/model/vqa.py
+++ travel/model/vqa.py
@@ -5,7 +5,7 @@ from PIL import Image
 import torch
 
 COMPLETION_PROMPT_TEMPLATES = {
-    "Salesforce/blip2-flan-t5-xxl": "A photo of",
+    "Salesforce/blip2-flan-t5-xxl": "A photo of <pad>",
     "Salesforce/instructblip-flan-t5-xxl": "A photo of",
     "microsoft/kosmos-2-patch14-224": "<grounding> A photo of",
     "llava-hf/llava-1.5-7b-hf": 'USER: <image>\nWhat is happening in this photo? ASSISTANT: This is a photo of',
@@ -14,16 +14,16 @@ COMPLETION_PROMPT_TEMPLATES = {
 }
 
 SUCCESSVQA_PROMPT_TEMPLATES = {
-    "Salesforce/blip2-flan-t5-xxl": 'Question: The current goal is "{step}". Has the person successfully finished doing this? Answer:',
+    "Salesforce/blip2-flan-t5-xxl": 'Question: The current goal is "{step}". Has the person successfully finished doing this? Answer: <pad>',
     "Salesforce/instructblip-flan-t5-xxl": 'Question: The current goal is "{step}". Has the person successfully finished doing this? Answer:',
-    "microsoft/kosmos-2-patch14-224": "<grounding> Q: The current goal is {step}. Has the person successfully finished doing this? A: ",
+    "microsoft/kosmos-2-patch14-224": '<grounding> Q: The current goal is "{step}". Has the person successfully finished doing this? A: ',
     "llava-hf/llava-1.5-7b-hf": 'USER: <image>\nThe current goal is "{step}". Has the person successfully finished doing this? ASSISTANT: ',
     "llava-hf/llava-v1.6-vicuna-7b-hf": 'A chat between a curious human and an artificial intelligence assistant. The assistant gives helpful, detailed, and polite answers to the human\'s questions. USER: <image>\nThe current goal is "{step}". Has the person successfully finished doing this? ASSISTANT:',
     "llava-hf/llava-v1.6-mistral-7b-hf": '[INST] <image>\nThe current goal is "{step}". Has the person successfully finished doing this? [/INST]',
 }
 
 VQG2VQA_PROMPT_TEMPLATES = {
-    "Salesforce/blip2-flan-t5-xxl": "Question: {question}? Answer: ",
+    "Salesforce/blip2-flan-t5-xxl": "Question: {question}? Answer: <pad>",
     "Salesforce/instructblip-flan-t5-xxl": "Question: {question}? Answer: ",
     "microsoft/kosmos-2-patch14-224": "<grounding> Question: {question} Answer: ",
     "llava-hf/llava-1.5-7b-hf": "USER: <image>\n{question} ASSISTANT: ",
