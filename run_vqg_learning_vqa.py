61a5dd5c73c9f81d50717b1bfd5e3b2991abd9b5
shanestorks <shanestorks@gmail.com>
2024-05-28 09:33:08 -0400

    Bug fix for partial saving of results

--- run_vqg_learning_vqa.py
+++ run_vqg_learning_vqa.py
@@ -130,8 +130,8 @@ for partition in args.generate_partitions:
 
             # Save partial progress in a supfolder
             chunks_results_dir = os.path.join(this_results_dir, f"chunks{first_chunk_idx}-{first_chunk_idx+len(frameVQA_examples_chunks)-1}")
-            save_vqa_outputs([output for sub_output in vqa_outputs for output in sub_output], this_results_dir, partition)
-            save_vqg_training_examples(vqg_training_examples, this_results_dir, partition)
+            save_vqa_outputs([output for sub_output in vqa_outputs for output in sub_output], chunks_results_dir, partition)
+            save_vqg_training_examples(vqg_training_examples, chunks_results_dir, partition)
 
         return vqg_training_examples, vqa_outputs
 
